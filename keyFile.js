8d93bbebfc398ebecbfcde19c1d9ce4ede39a8e6
Khalea <khaleaberry@gmail.com>
2019-08-16 00:04:58 -0400

    Deleted extra files

--- keyFile.js
+++
@@ -1,2 +0,0 @@
-console.log('hi')
-document.getElementById('scriptForKey').innerHTML = `<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDruMvSiWaX_UgV4j4N5qg3c4S9Yn8-Vsg&callback=initMap" type="text/javascript"></script>`
